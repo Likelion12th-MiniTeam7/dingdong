f8b1192d8384fd9acaf8e83034b329f964b3f773
naye0n00 <wendy3302@naver.com>
2024-05-23 03:41:21 +0900

    done

--- dingdong/settings.py
+++ dingdong/settings.py
@@ -35,7 +35,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['naye0n01.pythonanywhere.com']
+ALLOWED_HOSTS = ['dingd0ng.pythonanywhere.com']
 
 
 # Application definition
